923f1387849498e849569d8062d5ca7e82f4495f
Goncalo Ramalho <meet@goncaloramalho.com>
2020-12-07 22:00:20 +0000

    Setup project helpers - Add gatsby-plugin-root-import to define paths from root - Change prettier definitions

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,28 @@
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
-
+const path = require('path');
 module.exports = {
   /* Your site config here */
-  plugins: [],
-}
+  siteMetadata: {
+    title: 'Gonçalo Ramalho - Frontend Developer',
+    description:
+      'Hello my name is Gonçalo Ramalho and I’m a web enthusiast who likes to craft interesting and beautiful projecs for the web.',
+    author: '@filhodoto',
+    siteUrl: 'http://www.goncaloramalho.com',
+  },
+  plugins: [
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-typescript',
+    'gatsby-plugin-typescript-checker',
+    {
+      resolve: 'gatsby-plugin-root-import',
+      options: {
+        components: path.join(__dirname, 'src/components'),
+        helpers: path.join(__dirname, 'src/helpers'),
+        src: path.join(__dirname, 'src'),
+        pages: path.join(__dirname, 'src/pages'),
+      },
+    },
+  ],
+};
