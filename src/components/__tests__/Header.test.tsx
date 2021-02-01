5955864819c790b4599dcfecfb3ecc9e5d4352bd
Goncalo Ramalho <meet@goncaloramalho.com>
2021-02-01 15:44:16 +0000

    Add header tests

--- src/components/__tests__/Header.test.tsx
+++ src/components/__tests__/Header.test.tsx
@@ -1,14 +1,38 @@
 import * as React from 'react';
 import { render } from 'tests/test-utils';
-
+import { fireEvent } from '@testing-library/react';
 import Header from 'components/Header';
-import { darkTheme } from 'theme/theme';
+import { DefaultTheme } from 'styled-components/macro';
+
+// Render Header for testing
+const getHeaderEl = (props?: { theme: DefaultTheme }) =>
+  render(<Header {...props} />);
 
 describe(`Header tests`, () => {
   it(`renders theme switch`, () => {
-    const { getByTestId } = render(<Header />);
+    const { getByTestId } = getHeaderEl();
+    expect(getByTestId('switch')).toBeInTheDocument();
+  });
 
-    const swicth = getByTestId('switch');
-    expect(swicth).toBeInTheDocument();
+  it('Render dark mode as default', () => {
+    const { getByTestId } = getHeaderEl();
+
+    // Check if swicth label has dark mode text
+    expect(getByTestId('switch-label').textContent).toBe('too dark?');
+  });
+
+  it('Should change theme when switch is clicked', () => {
+    const { getByTestId } = getHeaderEl();
+    const switchEl = getByTestId('switch');
+    const switchLabel = getByTestId('switch-label');
+
+    // Confirm we are in dark mode by default
+    expect(switchLabel.textContent).toBe('too dark?');
+
+    // Trigger Switch click event
+    fireEvent.click(switchEl);
+
+    // Check if tesy changed to light more
+    expect(switchLabel.textContent).toBe('too light?');
   });
 });
