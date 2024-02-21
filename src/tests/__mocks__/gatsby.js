eb30edb55efe8c2a37c247dafd70d3c5069bd245
Gonçalo Ramalho <goncalo_ramalho@hotmail.com>
2024-02-21 10:00:40 +0000

    Update test configurations to use new packages versions

--- src/tests/__mocks__/gatsby.js
+++ src/tests/__mocks__/gatsby.js
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const React = require('react');
 const gatsby = jest.requireActual('gatsby');
 module.exports = {
